48daa26c0200b2c9faef5a0014a097e935609006
superjc1000 <121019849+superjc1000@users.noreply.github.com>
2023-02-28 13:09:43 +0100

    v2

--- Trending.js
+++ Trending.js
@@ -1,19 +1,20 @@
+const Trending = async () => {
+  let url_general =
+    "https://api.themoviedb.org/3/trending/all/day?api_key=75b5d3032fe9c1f4c8f28e1cc2858dfc";
+  const api = await fetch(url_general);
+  const data = await api.json();
+  console.log(data);
+};
 
-
-const Trending=async()=>{
-let url_general = "https://api.themoviedb.org/3/trending/all/day?api_key=75b5d3032fe9c1f4c8f28e1cc2858dfc"
-const api= await fetch(url_general);
-    const data=await api.json();
-    console.log(data);
-}
-
-const Find=async(external_id)=>{
-let url_general = "https://api.themoviedb.org/3/find/"+external_id+"?api_key=75b5d3032fe9c1f4c8f28e1cc2858dfc&language=en-US&external_source=imdb_id"
-const api= await fetch(url_general);
-    const data=await api.json();
-    console.log(data);
-}
-
+const Find = async (external_id) => {
+  let url_general =
+    "https://api.themoviedb.org/3/find/" +
+    external_id +
+    "?api_key=75b5d3032fe9c1f4c8f28e1cc2858dfc&language=en-US&external_source=imdb_id";
+  const api = await fetch(url_general);
+  const data = await api.json();
+  console.log(data);
+};
 
 Trending();
 Find(213);
