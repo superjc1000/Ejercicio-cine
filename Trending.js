ec7d3ed16a0f2cbdf05b39703fd92f3e538138a2
superjc1000 <121019849+superjc1000@users.noreply.github.com>
2023-02-28 12:27:02 +0100

    v1

--- Trending.js
+++ Trending.js
@@ -7,4 +7,13 @@ const api= await fetch(url_general);
     console.log(data);
 }
 
+const Find=async(external_id)=>{
+let url_general = "https://api.themoviedb.org/3/find/"+external_id+"?api_key=75b5d3032fe9c1f4c8f28e1cc2858dfc&language=en-US&external_source=imdb_id"
+const api= await fetch(url_general);
+    const data=await api.json();
+    console.log(data);
+}
+
+
 Trending();
+Find(213);
