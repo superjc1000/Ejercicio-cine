592086d2abf0cc87f427af54019ca27ff31fa66a
superjc1000 <121019849+superjc1000@users.noreply.github.com>
2023-03-01 12:40:39 +0100

    js trending fechas y nota

--- Trending.js
+++ Trending.js
@@ -15,8 +15,11 @@ const trending = async () => {
             }" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${item.title}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <p class="card-text">Fecha de salida: ${item.release_date}</p>
+                <p class="card-text">Nota media: ${item.vote_average}</p>
+                <a href="#${
+                  item.homepage
+                }" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
         `;
