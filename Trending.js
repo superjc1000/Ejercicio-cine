17192ca124e71ad14defbdcb9fbe04dc22cfbfc3
superjc1000 <121019849+superjc1000@users.noreply.github.com>
2023-03-01 12:12:53 +0100

    js va mejor (imagenes+nombres)

--- Trending.js
+++ Trending.js
@@ -9,9 +9,12 @@ const trending = async () => {
     divItem = document.createElement("div");
     divItem.innerHTML = `
         <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="${item.image}" alt="...">
+            <img class="card-img-top" src="${
+              "https://www.themoviedb.org/t/p/w440_and_h660_face" +
+              item.poster_path
+            }" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">${item.title}</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
