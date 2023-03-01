01a66984b5bb168e91c9b3ce5a52dc051660e28e
superjc1000 <121019849+superjc1000@users.noreply.github.com>
2023-03-01 11:46:29 +0100

    js trending "va"

--- Trending.js
+++ Trending.js
@@ -1,12 +1,27 @@
-const Trending = async () => {
+const trending = async () => {
   let url_general =
     "https://api.themoviedb.org/3/trending/all/day?api_key=75b5d3032fe9c1f4c8f28e1cc2858dfc";
   const api = await fetch(url_general);
   const data = await api.json();
   console.log(data);
+  div_cine = document.querySelector("#resultado");
+  data.results.map((item) => {
+    divItem = document.createElement("div");
+    divItem.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="${item.image}" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        `;
+    div_cine.appendChild(divItem);
+  });
 };
 
-const Find = async (external_id) => {
+const find = async (external_id) => {
   let url_general =
     "https://api.themoviedb.org/3/find/" +
     external_id +
@@ -16,5 +31,5 @@ const Find = async (external_id) => {
   console.log(data);
 };
 
-Trending();
-Find(937278);
+trending();
+find("937278");
